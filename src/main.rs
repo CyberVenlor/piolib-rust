f86f492adb015cf4fba4ede72f114a8851479381
CyberVenlor <badcat17@163.com>
2025-12-25 16:42:23 +0800

    Update Cargo.toml and main.rs for PIO integration; add VSCode settings

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
-fn main() {
-    pio_pi5_rs::
-    println!("Hello, world!");
+use std::{error::Error, process::Stdio};
+use pio_pi5_rs::*;
+
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let pio = Rp1PIO::new(0)?;
+    let state_machine = pio.sm_claim_unused()?;
+    let instructions = [0x6221, 0x1223, 0x1300, 0xa342];
+    let offset = pio.add_program(&PioProgram::new(&instructions, None));
+
+    Ok(())
 }
